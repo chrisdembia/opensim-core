536f9788044001a8ca8e87126261978b7d329285
Christopher Dembia <cld72@cornell.edu>
2014-07-05 14:31:43 -0700

    The start of a MultiCoordinateActuator.
    Issues:
    
    1. I am not sure that my definition of computeForce() is correct
    (looping through coordinates, getting the MobilizerQIndex).
    2. I am not accounting for locked or constrained coordinates.
    3. I do not make any use of the Actuator's optimal_force.

--- OpenSim/Actuators/RegisterTypes_osimActuators.cpp
+++ OpenSim/Actuators/RegisterTypes_osimActuators.cpp
@@ -27,6 +27,7 @@
 #include "RegisterTypes_osimActuators.h"
 
 #include "CoordinateActuator.h"
+#include "MultiCoordinateActuator.h"
 #include "PointActuator.h"
 #include "TorqueActuator.h"
 #include "BodyActuator.h"
@@ -78,6 +79,7 @@ OSIMACTUATORS_API void RegisterTypes_osimActuators()
   try {
 
 	Object::registerType( CoordinateActuator() );
+	Object::registerType( MultiCoordinateActuator() );
 	Object::registerType( PointActuator() );
 	Object::registerType( TorqueActuator() );
 	Object::registerType( BodyActuator() );
